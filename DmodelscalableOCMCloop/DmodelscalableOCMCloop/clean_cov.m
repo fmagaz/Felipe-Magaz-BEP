3aef1bef4a626b7b4cc95cae0820b99c23d42bab
fmagaz <f.magaz@student.tue.nl>
2026-06-30 00:30:17 +0200

    Delete DmodelscalableOCMCloop directory

--- DmodelscalableOCMCloop/DmodelscalableOCMCloop/clean_cov.m
+++
@@ -1,8 +0,0 @@
-function Pclean = clean_cov(P)
-    Pclean = (P + P')/2;
-    [V,D] = eig(Pclean);
-    d = real(diag(D));
-    d(d < 1e-10) = 1e-10;
-    Pclean = V*diag(d)*V';
-    Pclean = real((Pclean + Pclean')/2);
-end
--- DmodelscalableOCMCloop/DmodelscalableOCMCloop/info_mat.m
+++
@@ -1,5 +0,0 @@
-function Y = info_mat(P)
-    P = clean_cov(P);
-    Y = P \ eye(size(P));
-    Y = clean_cov(Y);
-end
